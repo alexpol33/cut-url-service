c5e03ba5117383388f84a1e48979f8e2c94689a0
Alexpol <alexpol2905@gmail.com>
2021-10-28 14:31:51 +0300

    creating file with functions

--- includes/config.php
+++ includes/config.php
@@ -1,2 +1,3 @@
 <?php
 define('SITE_NAME', 'Cut your URL');
+define('HOST', 'http://example-app');
---
+++ includes/functions.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'includes/config.php';
+
+function get_url($page = ''){
+    return HOST . "/$page";
+}
