41d49d62f9db6d18827d3d28bbcf92a99dbb392f
Alexpol <alexpol2905@gmail.com>
2021-10-29 14:16:35 +0300

    connect to db

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,11 @@
 <?php
 include 'includes/functions.php';
+
+$db = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME . "; charset=utf8", DB_USER, DB_PASS, [
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+])
 ?>
 <!doctype html>
 <html lang="ru">
